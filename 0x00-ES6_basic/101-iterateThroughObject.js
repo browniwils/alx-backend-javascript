f816bc31a761c43a135952bcdbb665ec4ee3f045
browniwils <browniwils@gmail.com>
2023-12-02 01:40:15 +0000

    completed project 0x00

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -3,7 +3,10 @@
 export default function iterateThroughObject(reportWithIterator) {
   let finalNames = ""
   for (const name of reportWithIterator) {
-    finalNames += name
+    finalNames += `${name}`;
+    if (indexOf(name) !== (reportWithIterator.length - 1)) {
+        finalNames += " | ";
+    }
   }
 
   return finalNames
